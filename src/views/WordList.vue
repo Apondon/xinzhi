1afd696b5673ff17e61c9f33224787ec4db37e95
apondon <apondon@126.com>
2021-07-15 11:29:02 +0800

    字典

--- src/views/WordList.vue
+++ src/views/WordList.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div.word
+div.wordList
     Bread(:target="target")
     el-form(:inline="true",:model="formInline",class="demo-form-inline")
         el-form-item(label="字典名称")
@@ -11,12 +11,12 @@ div.word
             el-button(type="primary",icon="el-icon-search",@click="onSubmit") 搜索
         el-form-item
             el-button(icon="el-icon-refresh-left",@click="resetHandle") 重置
-    div.wordBox
+    div.wordlistdBox
         el-button(type="primary",icon="el-icon-plus",plain,size="small",@click="addOne") 添加字典
         el-table(:data="tableData",style="width: 100%",height="600px",border="true")
             el-table-column(label="序号",type="index",width="50")
-            el-table-column(prop="name",label="字典名称",width="180")
-            el-table-column(prop="code",label="Code",width="180")
+            el-table-column(prop="name",label="数据标签",width="180")
+            el-table-column(prop="value",label="Value",width="180")
             el-table-column(prop="status",label="状态",width="180")
                 template(#default="scope")
                     span(v-show="scope.row.status===1") 正常
@@ -26,7 +26,6 @@ div.word
             el-table-column(label="操作",align="center")
                 template(#default="scope")
                     el-link.el-icon-edit(:underline="false",href="javascript:;",@click="editHandle(scope.$index,scope.row)") 编辑
-                    el-link.el-icon-s-fold(:underline="false",href="javascript:;",@click="linkToList(scope.row)") 列表
                     el-link.el-icon-delete-solid(:underline="false",href="javascript:;",@click="deleteHandle(scope.$index,scope.row)") 删除
         el-pagination(
             @size-change="handleSizeChange",
@@ -38,60 +37,63 @@ div.word
             :total="400") 
 el-dialog(:title="title",v-model="dialogVisible",width="30%",:center="false")
     el-form(ref="form",:model="form",label-width="80px")
-        el-form-item(label="字典名称")
-            el-input(v-model="form.name")
-        el-form-item(label="Code")
-            el-input(v-model="form.code")
-        el-form-item(label="状态")
-            el-radio(v-model="form.status",:label="1") 正常
-            el-radio(v-model="form.status",:label="2") 禁用
-        el-form-item(label="备注")
-            el-input(v-model="form.text")
+        //- el-form-item(label="字典名称")
+        //-     el-input(v-model="form.name")
+        //- el-form-item(label="Code")
+        //-     el-input(v-model="form.code")
+        //- el-form-item(label="状态")
+        //-     el-radio(v-model="form.status",:label="1") 正常
+        //-     el-radio(v-model="form.status",:label="2") 禁用
+        //- el-form-item(label="备注")
+        //-     el-input(v-model="form.text")
     template(#footer)
         div.dialog-footer
             el-button(@click="closeHandle") 关闭
             el-button(type="primary" @click="submitHandle") 提交
-      
 </template>
 
 <script>
 export default {
     data(){
         return{
-            target:['系统设置','字典管理'],
+            target:['系统管理','列表'],
             formInline:{
                 user:'',
                 radio:1,
             },
+            id:'',
             tableData:[],
             tableList:[
                 {
-                    id:1,
-                    name:'字典1',
-                    code:123456,
+                    group:1,
+                    name:'标签1',
+                    value:'111',
                     status:1,
-                    text:'正常字典',
-                    time:'2021-07-14 10:30:10'
+                    text:'这是标签1',
+                    time:'2021-07-15 09:13:01'
                 },
                 {
-                    id:2,
-                    name:'字典2',
-                    code:134567,
+                    group:1,
+                    name:'标签11',
+                    value:'111',
                     status:2,
-                    text:'不正常字典',
-                    time:'2021-07-14 10:31:10'
+                    text:'这是标签1',
+                    time:'2021-07-15 09:13:01'
+                },
+                {
+                    group:2,
+                    name:'标签2',
+                    value:'222',
+                    status:2,
+                    text:'这是标签1',
+                    time:'2021-07-15 09:13:01'
                 }
             ],
             currentPage4:1,
+            // dialog
             dialogVisible:false,
             isEdit:false,
             title:'',
-            form:{
-                name:'',
-                code:'',
-                status:0,
-                text:'',
-            },
             editItem:0,
         }
     },
@@ -105,67 +107,29 @@ export default {
             }
         },
         resetHandle(){
-            this.tableData = this.tableList
+            this.tableData = []
+            for(let i = 0;i < this.tableList.length;i++){
+            if(this.tableList[i].group == this.id){
+                this.tableData.push(this.tableList[i])
+                }
+            }
             this.formInline = {
                 user:'',
                 radio:1,
             }
-        },
-        handleSizeChange(val){
-            console.log(`每页 ${val} 条`)
-        },
-        handleCurrentChange(val){
-            console.log(`当前页: ${val}`)
         },
         addOne(){
             this.title = '添加角色'
             this.dialogVisible = true
             this.isEdit = false
         },
-        closeHandle(){
-            this.dialogVisible = false
-        },
-        submitHandle(){
-            if(this.isEdit){
-                this.tableData[this.editItem] = {
-                    name:this.form.name,
-                    code:this.form.code,
-                    status:this.form.status,
-                    text:this.form.text,
-                    time:this.transTime()
-                }
-            }else{
-                this.tableData.push({
-                    name:this.form.name,
-                    code:this.form.code,
-                    status:this.form.status,
-                    text:this.form.text,
-                    time:this.transTime()
-                })
-            }
-            this.form = {
-                name:'',
-                code:'',
-                status:'',
-                text:'',
-                time:''
-            }
-            this.dialogVisible = false
-        },
         deleteHandle(index , row){
             this.tableData.splice(index , 1)
         },
         editHandle(index,row){
-            this.title = '编辑字典'
-            this.editItem = index
-            this.isEdit = true
-            this.form = {
-                name:row.name,
-                code:row.code,
-                status:row.status,
-                text:row.text,
-            }
+            this.title = '编辑角色'
             this.dialogVisible = true
+            this.isEdit = true
         },
         transTime(){
             let D = new Date()
@@ -177,26 +141,61 @@ export default {
             let ss = D.getSeconds() < 10 ? '0' + D.getSeconds() : D.getSeconds()
             return `${yyyy}-${MM}-${dd} ${hh}:${mm}:${ss}`
         },
-        linkToList(row){
-
-            this.$router.push({path:'/wordList',query:{id:row.id}})
-        }
+        // dialog
+        closeHandle(){
+            this.dialogVisible = false
+        },
+        submitHandle(){
+            // if(this.isEdit){
+            //     this.tableData[this.editItem] = {
+            //         name:this.form.name,
+            //         value:this.form.value,
+            //         status:this.form.status,
+            //         text:this.form.text,
+            //         time:this.transTime()
+            //     }
+            // }else{
+            //     this.tableData.push({
+            //         group:this.id,
+            //         name:this.form.name,
+            //         value:this.form.value,
+            //         status:this.form.status,
+            //         text:this.form.text,
+            //         time:this.transTime()
+            //     })
+            // }
+            this.form = {
+                name:'',
+                value:'',
+                status:'',
+                text:'',
+                time:''
+            }
+            this.dialogVisible = false
+        },
     },
     mounted(){
-        this.tableData = this.tableList
+        let id = this.$route.query.id
+        this.id = id
+        // this.tableData = this.tableList
+        for(let i = 0;i < this.tableList.length;i++){
+            if(this.tableList[i].group == id){
+                this.tableData.push(this.tableList[i])
+            }
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-.word{
+.wordList{
     box-sizing: border-box;
     padding:20px 45px;
     .el-form{
         margin-top: 20px;
         text-align: left;
     }
-    .wordBox{
+    .wordlistdBox{
         margin-top: 20px;
         text-align: left;
     }
